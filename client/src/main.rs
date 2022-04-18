03253bb4fda2b4d5fc26543882c91b346b140ce5
Rajput, Rajat <Rajat.Rajput@aristocrat.com>
2022-04-18 23:01:50 +0530

    add another import

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use hello::{greeter_client::GreeterClient, HelloRequest};
+use tonic::{transport::Error as TError, Request};
+
+pub mod hello {
+    tonic::include_proto!("hello");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), CustomError> {
+    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+    let req = Request::new(HelloRequest {
+        name: "drink this.".to_owned(),
+    });
+    let resp = client.say_hello(req).await;
+    println!("response: {:?}", resp);
+    Ok(())
+}
+
+#[derive(Debug)]
+enum CustomError {
+    Other(String),
+    TonicTransport(TError),
+}
+
+impl From<TError> for CustomError {
+    fn from(e: TError) -> Self {
+        CustomError::TonicTransport(e)
+    }
 }
