0aeb87bc0ffbc19f7d2f589d962d4a091a20446d
Rajput, Rajat <Rajat.Rajput@aristocrat.com>
2022-04-19 11:31:08 +0530

    should listen on org-ip now

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,9 @@
-use std::net::{AddrParseError, SocketAddr};
-
 use hello::{
     greeter_server::{Greeter, GreeterServer},
     HelloReply, HelloRequest,
 };
+use local_ip_address::list_afinet_netifas;
+use std::net::{AddrParseError, IpAddr, SocketAddr};
 use tonic::{
     transport::{Error as TError, Server},
     Request, Response, Status,
@@ -26,9 +26,22 @@ impl Greeter for MyGreeeterServer {
     }
 }
 
+fn get_ip() -> Result<IpAddr, CustomError> {
+    list_afinet_netifas()?
+        .iter()
+        .find_map(|v| {
+            if v.1.to_string().contains("172.") {
+                Some(v.1)
+            } else {
+                None
+            }
+        })
+        .ok_or_else(|| CustomError::OrgIpNotListed)
+}
+
 #[tokio::main]
 async fn main() -> Result<(), CustomError> {
-    let addr = "[::]:50051".parse::<SocketAddr>()?;
+    let addr = format!("{}:50051", get_ip()?).parse::<SocketAddr>()?;
     let greeter = MyGreeeterServer;
     println!("listening on '{}'", addr);
     Server::builder()
@@ -42,6 +55,8 @@ async fn main() -> Result<(), CustomError> {
 enum CustomError {
     TonicTransport(TError),
     SockerParser(AddrParseError),
+    LocalIp(local_ip_address::Error),
+    OrgIpNotListed,
 }
 
 impl From<TError> for CustomError {
@@ -53,5 +68,11 @@ impl From<TError> for CustomError {
 impl From<AddrParseError> for CustomError {
     fn from(e: AddrParseError) -> Self {
         CustomError::SockerParser(e)
+    }
+}
+
+impl From<local_ip_address::Error> for CustomError {
+    fn from(e: local_ip_address::Error) -> Self {
+        CustomError::LocalIp(e)
     }
 }
