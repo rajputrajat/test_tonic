17e5c27703571854ef72c7aa4e223deab61723ad
Rajput, Rajat <Rajat.Rajput@aristocrat.com>
2022-04-19 11:12:48 +0530

    let's see if this works too

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,5 @@
 use hello::{greeter_client::GreeterClient, HelloRequest};
+use std::env;
 use tonic::{transport::Error as TError, Request};
 
 pub mod hello {
@@ -7,7 +8,9 @@ pub mod hello {
 
 #[tokio::main]
 async fn main() -> Result<(), CustomError> {
-    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+    let addr = env::args().skip(1).collect::<Vec<String>>();
+    assert_eq!(addr.len(), 2, "pass <ip-addr> and <port>");
+    let mut client = GreeterClient::connect(format!("http://{}:{}", addr[0], addr[1])).await?;
     let req = Request::new(HelloRequest {
         name: "I am client. Hello server!".to_owned(),
     });
